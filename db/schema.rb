39e91433299f87a0b62490630a7e0856774c86b4
ajaychauhan1802 <ajaychauhan1802@gmail.com>
2016-05-21 23:21:30 -0400

    added quantity field to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160521225632) do
+ActiveRecord::Schema.define(version: 20160522025259) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160521225632) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "quantity"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create(:name => "Red Poha", :description => "Organic Flattened Rice, used to prepare snack or light easy fast food")
+Product.create(:name => "Oat Flakes", :description => "Rolled oats, excellent source of Thianmine, Iron, Proteins and Diatery Fibers")
+Product.create(:name => "Wheatgerm", :description => "Very high in protein")
+Product.create(:name => "Orange Garnola", :description => "Blend of soft baked oats, almond and orange zest")
+
