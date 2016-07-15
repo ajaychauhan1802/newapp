ae9946202fc83c568b958bbfb71a9dcd795d052c
ajaychauhan1802 <ajaychauhan1802@gmail.com>
2016-07-14 21:15:14 -0400

    comment rating

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
+  
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  resources :products do
+    resources :comments
+  end
   resources :users
-  resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
+  
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
+
+  get 'static_pages/product'
 
   post 'static_pages/thank_you'
 
