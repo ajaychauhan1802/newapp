c55cb07a119e6aaed5c8003cddd97ad6c9880643
ajaychauhan1802 <ajaychauhan1802@gmail.com>
2016-07-14 21:31:08 -0400

    validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,9 @@ def average_rating
   comments.average(:rating).to_f
 end
 
+validates :name, presence: true
+validates :user, presence: true
+validates :product, presence: true
+validates :rating, numericality: {only_integer: true}
+
 end
